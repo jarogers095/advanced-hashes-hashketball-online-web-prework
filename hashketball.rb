ccd115cf3acc0eb8e5ca420800a92a50d8e9bbc3
James Rogers <jarogers095@gmail.com>
2019-12-05 03:54:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -43,7 +43,7 @@ end
 def team_colors(team_name)
   if (game_hash[:home][:team_name] == team_name)
     return game_hash[:home][:colors]
-  elsif ((game_hash[:away][:team_name] == team_name)
-    return game_hash[:away][:colors])
+  elsif (game_hash[:away][:team_name] == team_name)
+    return game_hash[:away][:colors]
   end
 end
