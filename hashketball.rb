52381b31fb4eacecab4053dec4fef82996bbbb5c
James Rogers <jarogers095@gmail.com>
2019-12-05 03:50:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,14 +12,22 @@ def game_hash()
 end
 
 def num_points_scored(name)
-  player_holder = []
-  game_hash[:home][:players].each do |player|
-    if (player[:player_name] == name)
+  get_array_of_players.each do |player|
+    if player[:player_name] == name
+      return player[:points]
+    end
   end
 end
 
 def get_array_of_players() 
   player_holder = []
+  
   game_hash[:home][:players].each do |player|
     player_holder.push(player)
   end
+  game_hash[:away][:players].each do |player|
+    player_holder.push(player)
+  end
+  
+  return player_holder
+end
